fc9bf366f819c00d533627a47df187e653396d4e
cristivlas <cristi.vlasceanu@gmail.com>
2017-06-20 19:37:04 -0700

    use timestamp when logging, experimenting with fetching currents data

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+//app.use(logger('dev'));
+app.use(logger('[:date[iso]] :remote-addr :method :url :status :response-time ms'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
