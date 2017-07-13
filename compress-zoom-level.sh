3599d224df754b4d7a0bc43b5834a02ac594556c
cristivlas <cristi.vlasceanu@gmail.com>
2017-07-13 08:49:48 -0700

    improved tiles compression script

--- compress-zoom-level.sh
+++ compress-zoom-level.sh
@@ -1,10 +1,19 @@
 #! /usr/bin/env bash
 set -f
-zoom=${1:-*}
+zoom=$1
+output=tiles.`hostname`.`date +%Y%m%d%H%M`.tgz
+if [ -z $zoom ]; then
+  zoom=*
+else
+  output=tiles-${zoom}.`hostname`.`date +%Y%m%d%H%M`.tgz
+fi
 
 pattern=tiles/*/*/*.
 pattern+=$zoom
 pattern+=.*.*.png
 
+echo $output
+echo $pattern
+
 set +f
-tar zvcf tiles.`hostname`.`date +%Y%m%d%H%M`.tgz ${pattern}
+tar zvcf ${output} ${pattern}
