00bd9b60c6109cd032754c34c2b630b9b597f4a6
cristivlas <cristi.vlasceanu@gmail.com>
2017-08-16 21:40:36 -0700

    compass fixes

--- public/javascripts/geolocation.js
+++ public/javascripts/geolocation.js
@@ -98,6 +98,7 @@ class Geolocation {
     }
   }
 
+
   _useHTML5Geolocation() {
     if (this._mobile && !this._once) {
       this._once = true;
@@ -110,9 +111,9 @@ class Geolocation {
           if (!e.absolute || e.alpha === null) {
             return;
           }
-          this._heading = compassHeading(e.alpha, e.beta, e.gamma);
+          this._heading = -e.alpha;
         }
-        this.rotation = this._heading * (Math.PI / 180);
+        this._updateHeading();
         if (this._compassCallback) {
           this._compassCallback(this);
         }
@@ -163,13 +164,17 @@ class Geolocation {
     }
   }
 
-  _update(coord) {
-    if (!coord.heading && this._heading) {
-      coord.heading = this._heading;
+  _updateHeading() {
+    if (this._heading) {
+      this.coord.heading = this._heading + window.orientation;
+      this.rotation = this.coord.heading * (Math.PI / 180);
     }
+  }
+
+  _update(coord) {
     this.coord = coord;
     this.speed = coord.speed;
-    this.rotation = (coord.heading % 2 * Math.PI) * (Math.PI / 180);
+    this._updateHeading();
 
     if (this._successCallback) {
       this._successCallback(coord);
@@ -177,40 +182,3 @@ class Geolocation {
   }
 }
 
-
-// https://stackoverflow.com/questions/18112729/calculate-compass-heading-from-deviceorientation-event-api
-function compassHeading(alpha, beta, gamma) {
-
-  // Convert degrees to radians
-  var alphaRad = alpha * (Math.PI / 180);
-  var betaRad = beta * (Math.PI / 180);
-  var gammaRad = gamma * (Math.PI / 180);
-
-  // Calculate equation components
-  var cA = Math.cos(alphaRad);
-  var sA = Math.sin(alphaRad);
-  var cB = Math.cos(betaRad);
-  var sB = Math.sin(betaRad);
-  var cG = Math.cos(gammaRad);
-  var sG = Math.sin(gammaRad);
-
-  // Calculate A, B, C rotation components
-  var rA = - cA * sG - sA * sB * cG;
-  var rB = - sA * sG + cA * sB * cG;
-  var rC = - cB * cG;
-
-  // Calculate compass heading
-  var compassHeading = Math.atan(rA / rB);
-
-  // Convert from half unit circle to whole unit circle
-  if(rB < 0) {
-    compassHeading += Math.PI;
-  }else if(rA < 0) {
-    compassHeading += 2 * Math.PI;
-  }
-
-  // Convert radians to degrees
-  compassHeading *= 180 / Math.PI;
-
-  return compassHeading;
-}
